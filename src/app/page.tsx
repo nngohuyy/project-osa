fe12c95eb660a9d538f7b621b01b301fbe0abc93
Huy Nguyen Ngo <huy.nguyenngo13@hcmut.edu.vn>
2024-06-20 15:10:11 +0700

    Modify homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,36 +16,33 @@ export default function Home() {
     <main className={`${gruppo.variable} translate-y-[-74px] pb-40 flex flex-col space-y-40`}>
       <section className="home-top-card flex flex-col justify-center h-[100dvh]">
         <div className="max-w-screen-xl mx-auto px-6 md:px-14 xl:px-14">
-          <div className="flex flex-col gap-10 w-2/3">
-            <h1 className="font-bold text-4xl md:text-6xl lg:text-7xl">We are<br/>OISP Student<br/>Ambassadors.</h1>
+          <div className="flex flex-col gap-5 md:w-2/3">
+            <h1 className="font-bold text-5xl md:text-6xl lg:text-7xl">We are<br/>OISP Student<br/>Ambassadors.</h1>
             <p>
-              We are a group of enthusiastic students who are passionate about
-              promoting the image of OISP students. We are here to support you in your journey at HCMUT.
+              We are a group of enthusiastic students who are passionate about promoting the image of OISP students. We are here to support you in your journey at HCMUT.
             </p>
           </div>
-          <Button className="mt-5" size="lg" color="primary">
+          <button className={`${gruppo.variable} mt-10`}>
             Learn more
-          </Button>
+          </button>
         </div>
       </section>
 
-      <section className="max-w-screen-xl mx-auto px-6 md:px-14 xl:px-14 flex flex-col md:flex-row">
-        <div className="w-1/2">
+      <section className="max-w-screen-xl mx-auto px-6 md:px-14 xl:px-14 flex flex-col gap-10 md:space-y-0 md:flex-row">
+        <div className="md:w-1/2">
           <h1 className="home-section-heading">Our mission</h1>
-          <p>
-            We aim at promoting the image of OISP students and being a bridge
-            between OISP and students, between Vietnamese students and
-            international student, and between students and employers.
+          <p className="text-center md:text-left">
+            We aim at promoting the image of OISP students and being a bridge between OISP and students, between Vietnamese students and international student, and between students and employers.
           </p>
         </div>
-        <div className="w-1/2 h-[200px] bg-gray-300 rounded-xl">
+        <div className="md:w-1/2 h-[200px] bg-gray-300 rounded-xl">
           {/* insert image here */}
         </div>
       </section>
 
       <section className="max-w-screen-xl mx-auto px-6 md:px-14 xl:px-14 w-full">
-        <h1 className="home-section-heading text-center">Our team</h1>
-        <div className="grid grid-cols-3 gap-4 h-[500px]">
+        <h1 className="home-section-heading !text-center">Our team</h1>
+        <div className="grid md:grid-cols-3 gap-4 h-[500px]">
           <div className="h-full w-full bg-gray-300 rounded-xl">
             {/* insert team members here */}
           </div>
@@ -58,20 +55,20 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="max-w-screen-xl mx-auto px-6 md:px-14 xl:px-14 w-full flex flex-col md:flex-row gap-10">
-        <div className="w-1/2 min-h-96 bg-gray-300 rounded-xl">
+      <section className="max-w-screen-xl mx-auto px-6 md:px-14 xl:px-14 w-full flex flex-col-reverse flex-col md:flex-row gap-10">
+        <div className="md:w-1/2 min-h-96 bg-gray-300 rounded-xl">
           {/* insert image here */}
         </div>
-        <div className="w-1/2">
+        <div className="md:w-1/2">
           <h1 className="home-section-heading">Our statement</h1>
           <div className="flex flex-col space-y-5">
-            <p>
+            <p className="text-center md:text-left">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
-            <p>
+            <p className="text-center md:text-left">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
-            <p>
+            <p className="text-center md:text-left">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </p>
           </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,11 +15,17 @@ import { OSALogo } from "./OSALogo";
 import { ROUTES } from "../constants/navigationRoutes";
 
 export default function NavBar() {
+  const openMobileMenu = () => {
+    document.getElementById("MobileMenu").classList.toggle("hidden");
+  }
+  const closeMobileMenu = () => {
+    document.getElementById("MobileMenu").classList.add("hidden");
+  }
   return (
     <nav className="bg-white border shadow-solid border-black rounded-2xl sticky top-3 z-50 max-w-screen-xl w-[calc(100%-24px)] mx-auto">
-      <div className="max-w-screen-xl mx-auto px-6 md:px-14 xl:px-14 py-4 flex justify-between items-center">
+      <div id='NavBar' className="max-w-screen-xl mx-auto px-6 md:px-14 xl:px-14 py-4 flex justify-between items-center">
         <div className="flex items-center">
-          <Link href="/">
+          <Link onPressStart={closeMobileMenu} href="/">
             <OSALogo />
           </Link>
         </div>
@@ -30,9 +36,34 @@ export default function NavBar() {
             </Link>
           ))}
         </div>
-        <div className="flex items-center space-x-4">
+        <div className="hidden md:flex items-center space-x-4">
           <Link href="/contact">
             <button className={gruppo.variable}>
+              Contact us
+            </button>
+          </Link>
+        </div>
+        <div className='flex md:hidden'>
+          <Button
+            className="md:hidden !shadow-none translate-y-0 translate-x-4 border-none !px-0 !py-0 hover:bg-inherit hover:shadow-none hover:text-inherit"
+            variant='light'
+            radius='full'
+            onPressStart={openMobileMenu}
+          >
+            <i className="pi pi-bars" style={{ fontSize: '1.75rem' }}></i>
+          </Button>
+        </div>
+      </div>
+      <div id='MobileMenu' className="block md:hidden bg-white border shadow-solid border-black rounded-2xl fixed top-[98px] z-50 max-w-screen-xl w-[calc(100%-24px)] mx-auto">
+        <div className='max-w-screen-xl mx-auto px-6 md:px-14 xl:px-14 py-20 flex flex-col space-y-4 items-center'>
+          {ROUTES.navigationRoutes.map((route) => (
+            <Link className={`${gruppo.className} text-xl`} key={route} href
+              ={`/${route}`}>
+              {route.charAt(0).toUpperCase() + route.slice(1)}
+            </Link>
+          ))}
+          <Link href="/contact">
+            <button className={`${gruppo.variable} mt-2`}>
               Contact us
             </button>
           </Link>
