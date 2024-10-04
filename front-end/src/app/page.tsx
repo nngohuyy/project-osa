9f0ead459af2254fd88d440af360b5f587ef3bef
Huy Nguyen Ngo <huy.nguyenngo13@hcmut.edu.vn>
2024-10-04 18:08:34 +0700

    Add SAMPLE to DESCRIPTION

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { Space_Grotesk } from 'next/font/google';
 import MemberCard from "../components/MemberCard";
 
-import { DESCRIPTION } from '@constants/descriptions';
+import { DESCRIPTION, SAMPLE } from '@constants/descriptions';
 
 const gruppo = Space_Grotesk({
   weight: "700",
@@ -73,13 +73,13 @@ export default function Home() {
           <h1 className="home-section-heading">Our statement</h1>
           <div className="flex flex-col space-y-5">
             <p className="text-center md:text-left">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              {SAMPLE.lorem_70}
             </p>
             <p className="text-center md:text-left">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              {SAMPLE.lorem_70}
             </p>
             <p className="text-center md:text-left">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              {SAMPLE.lorem_70}
             </p>
           </div>
         </div>
--- front-end/src/constants/descriptions.ts
+++ front-end/src/constants/descriptions.ts
@@ -1,7 +1,17 @@
-export const DESCRIPTION = {
+const DESCRIPTION = {
   about_us_long: "Established in 2015, the OISP Student Ambassadors Club actively promotes the positive image of OISP students. The club acts as a bridge between OISP and the student body, fostering connections between Vietnamese and international students, and facilitating engagement between students and potential employers.",
 
   about_us_short: "We aim at promoting the image of OISP students and being a bridge between OISP and students, between Vietnamese students and international student, and between students and employers.",
 
   social_media: "Follow us on social media!",
-}
+};
+
+const SAMPLE = {
+  lorem_30: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+
+  lorem_70: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+  lorem_150: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+};
+
+export {DESCRIPTION, SAMPLE};
