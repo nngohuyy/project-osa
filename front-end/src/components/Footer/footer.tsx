d599c130ac06823e7c751a6a9ee28955109bed82
Huy Nguyen Ngo <huy.nguyenngo13@hcmut.edu.vn>
2024-10-08 17:21:27 +0700

    Edit Footer

--- front-end/src/components/Footer/footer.tsx
+++ front-end/src/components/Footer/footer.tsx
@@ -8,39 +8,41 @@ const currentYear = new Date().getFullYear();
 export default function Footer() {
   return (
     <main className='bg-white'>
-      <div className="max-w-screen-xl mx-auto px-6 md:px-14 xl:px-14 py-4 xl:py-10">
-        <div className="section-about">
-          <div>
-            <h3 className="xl:mr-60 font-normal">
-              OISP Student Ambassadors, established in 2015, aims at promoting the image of OISP students and being a bridge between OISP and students, between Vietnamese students and international student, and between students and employers.
-            </h3>
-            <div className='section-social mt-10'>
-              <h3 className='font-bold'>Follow us on social media!</h3>
-              <div className="flex gap-4 mt-2">
-                <a href="https://www.facebook.com/oisp.student.ambassadors" className="text-2xl">
-                  <i className="pi pi-facebook text-[2rem] hover:text-[#1877F2]"></i>
-                </a>
-                <a href="https://www.instagram.com/osaxoi/" className="text-2xl">
-                  <i className="pi pi-instagram text-[2rem] hover:text-[#E1306C]"></i>
-                </a>
-                <a href="https://www.youtube.com/@oispstudentambassadors2015" className="text-2xl">
-                  <i className="pi pi-youtube text-[2rem] hover:text-[#FF0000]"></i>
-                </a>
+      <div className='max-w-screen-xl mx-auto px-14 py-10'>
+        <div className='flex flex-col gap-6'>
+          <div className='flex flex-row justify-between gap-[240px]'>
+            <div className='flex flex-col gap-10'>
+              <p>
+                Established in 2015, the OISP Student Ambassadors Club actively promotes the positive image of OISP students. The club acts as a bridge between OISP and the student body, fostering connections between Vietnamese and international students, and facilitating engagement between students and potential employers.
+              </p>
+              <div>
+                <p className='font-bold'>Follow us on social media!</p>
+                <div className="flex gap-4 mt-2">
+                  <a href="https://www.facebook.com/oisp.student.ambassadors" className="text-2xl">
+                    <i className="pi pi-facebook text-[2rem] hover:text-[#1877F2]"></i>
+                  </a>
+                  <a href="https://www.instagram.com/osaxoi/" className="text-2xl">
+                    <i className="pi pi-instagram text-[2rem] hover:text-[#E1306C]"></i>
+                  </a>
+                  <a href="https://www.youtube.com/@oispstudentambassadors2015" className="text-2xl">
+                    <i className="pi pi-youtube text-[2rem] hover:text-[#FF0000]"></i>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          <div className='osa-logo'>
             <Image
               src={logo}
               alt="OSA Logo"
+              className='h-[100px]'
             />
           </div>
+          <div>
+            <div className='h-1 bg-[#E4E4E7]'></div>
+            <p className="mt-1 font-normal">
+              Copyright © {currentYear} OISP Student Ambassadors. All rights reserved.
+            </p>
+          </div>
         </div>
-        <div className="line mt-6">
-        </div>
-        <h3 className="mt-1 font-normal">
-          Copyright © {currentYear} OISP Student Ambassadors. All rights reserved.
-        </h3>
       </div>
     </main>
   );
